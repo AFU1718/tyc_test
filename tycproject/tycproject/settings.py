6a722caa07ce9a9542cf9ed6691cc4418b72a6ad
AFU1718 <1390301315@qq.com>
2018-09-14 00:20:37 +0800

    初始化4

--- tycproject/tycproject/settings.py
+++ tycproject/tycproject/settings.py
@@ -91,7 +91,6 @@ COOKIES_ENABLED = False
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
 DOWNLOAD_DELAY = 3
-
 USER_AGENTS  = [
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 "
         "(KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
